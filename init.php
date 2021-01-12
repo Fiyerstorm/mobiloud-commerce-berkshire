9e98a4c1f4a0ca9eef967a0c19e881c199506779
Siddharth Thevaril <siddharththevaril@Siddharths-MacBook-Pro.local>
2021-01-12 13:51:02 +0530

    fix: product categories css and categories template

--- init.php
+++ init.php
@@ -5,6 +5,7 @@
 require_once 'app/assets.php';
 require_once 'app/hooks.php';
 require_once 'app/product-category.php';
+require_once 'app/product-categories.php';
 require_once 'app/single-product.php';
 require_once 'app/home.php';
 require_once 'app/cart.php';
