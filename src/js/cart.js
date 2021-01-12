e5a30c296aa7818802be18956542ef9b70e9bd43
Siddharth Thevaril <siddharththevaril@Siddharths-MacBook-Pro.local>
2021-01-12 20:04:39 +0530

    feat: add cart update logic

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,20 +1,71 @@
 ( function( $ ) {
+	const applyCouponBtn = $( 'button[name="apply_coupon"]' );
+	const applyCouponBtnText = $( '.mlwoo--cart__apply-coupon-text' );
+	const applyCouponBtnSpinner = $( '.mlwoo--cart__apply-coupon-spinner' );
+
+	let textCache;
 	let htmlCache;
-	let bgColor = '';
-	let fgColor = '';
+	let currentCartTotal = getCartTotal();
 
-	$( document ).on( 'click', 'button[name="apply_coupon"]', function() {
-		$( '.mlwoo--cart__apply-coupon-spinner' ).show();
-		fgColor = $( this ).css( 'color' );
-		bgColor = $( this ).css( 'background-color' );
+	applyCouponBtn.on( 'click', function() {
+		applyCouponBtnText.hide();
+		applyCouponBtnSpinner.show();
 
-		$( this ).css( 'color', bgColor );
+		textCache = this.childNodes[2].textContent;
 		htmlCache = $( this ).children();
+	} );
+	
+	$( document ).on( 'click', '.mlwoo--cart__quantity-input-control', function() {
+		const current = $( this );
+		const action = current.data( 'quantity-action' );
+		const closestParent = current.closest( '.mlwoo--cart__item-quantity' );
+		const inputField = closestParent.find( '.mlwoo--cart__quantity-input' );
+		const quantity = Number( inputField.val() );
+		
+		$( 'button[name="update_cart"]' ).prop( 'disabled', false );
+
+		switch ( action ) {
+			case 'increase':
+				inputField.val( quantity + 1 );
+				break;
+
+			case 'decrease':
+				if ( quantity > 1 ) {
+					inputField.val( quantity - 1 );
+				}
+				break;
+
+			default:
+				break;
+		}
 	} );
 
 	$( document ).on( 'applied_coupon', function() {
-		$( 'button[name="apply_coupon"]' ).append( htmlCache );
-		$( 'button[name="apply_coupon"]' ).css( 'color', fgColor );
-		$( '.mlwoo--cart__apply-coupon-spinner' ).hide();
+		applyCouponBtn.text( textCache ).append( htmlCache );
 	} );
+
+	$( document.body ).on( 'updated_cart_totals', function( event ) {
+		const updatedCartTotal = getCartTotal();
+	
+		if ( updatedCartTotal > currentCartTotal ) {
+			nativeFunctions.addToCart( updatedCartTotal - currentCartTotal );
+		}
+	
+		if ( updatedCartTotal < currentCartTotal ) {
+			nativeFunctions.removeFromCart( currentCartTotal - updatedCartTotal );
+		}
+
+		currentCartTotal = updatedCartTotal;
+	} );
+
+	function getCartTotal() {
+		let totalItems = 0;
+		const qtyInputFields = $( '.mlwoo--cart__quantity-input' );
+
+		qtyInputFields.each( function( index, item ) {
+			totalItems += Number( $( item ).val() );
+		} )
+
+		return totalItems;
+	}
 } )( jQuery )
