d9d4a41ea09ecac17a978bc37568dbb13664f252
Siddharth Thevaril <siddharththevaril@Siddharths-MacBook-Pro.local>
2021-03-29 18:46:47 +0530

    fix: remove redundant files

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,8 @@ const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
 module.exports = {
 	entry: {
-		'single-product': [ './src/js/single-product.js', './src/scss/single-product.scss' ],
-		// 'checkout': [ './src/js/checkout.js' ],
-		'cart': [ './src/js/cart.js', './src/scss/cart.scss' ],
-		// 'loop': [ './src/js/loop.js', './src/scss/loop.scss' ],
 		'checkout': [ './src/js/checkout.js', './src/scss/checkout.scss' ],
 		'account': [ './src/js/account.js', './src/scss/account.scss' ],
-		'account-orders': [ './src/js/orders.js', './src/scss/orders.scss' ],
-		'thank-you': [ './src/js/thank-you.js', './src/scss/thank-you.scss' ],
-		'account-view-order': [ './src/scss/view-order.scss' ],
-		// 'view-order': [ './src/scss/view-order.scss' ],
-		'account-edit-address': [ './src/js/edit-address.js', './src/scss/edit-address.scss' ],
-		'home': [ './src/js/home.js', './src/scss/home.scss' ],
-		// 'single-post': [ './src/scss/single-post.scss' ],
 		'single-post': [ './src/scss/single-post.scss' ],
 		'product-categories': [ './src/js/product-categories.js', './src/scss/product-categories.scss' ],
 		'product-category': [ './src/js/product-category.js', './src/scss/product-category.scss' ],
